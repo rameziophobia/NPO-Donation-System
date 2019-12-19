86bb846ff9552737336c144670df3750a015d008
Sameh Amnoun <samehamnoun@gmail.com>
2019-12-20 00:15:04 +0200

    fix: changed UI elements names
    Changed the name of all UI elements to Hungarian form (ex: btn_donate.)

--- NPODS/NPODS Non-Profit Organizations Donation System/organizationInfo.Designer.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/organizationInfo.Designer.cs
@@ -30,121 +30,121 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(organizationInfo));
             this.panel2 = new System.Windows.Forms.Panel();
-            this.dontatedLbl = new System.Windows.Forms.Label();
-            this.goalLbl = new System.Windows.Forms.Label();
-            this.descriptionLbl = new System.Windows.Forms.Label();
-            this.goalBar = new System.Windows.Forms.ProgressBar();
+            this.lbl_donated = new System.Windows.Forms.Label();
+            this.lbl_goal = new System.Windows.Forms.Label();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.pbr_goal = new System.Windows.Forms.ProgressBar();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.donateBtn = new System.Windows.Forms.Button();
-            this.orgPic = new System.Windows.Forms.PictureBox();
+            this.btn_donate = new System.Windows.Forms.Button();
+            this.pic_orgLogo = new System.Windows.Forms.PictureBox();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.orgPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_orgLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.Controls.Add(this.dontatedLbl);
-            this.panel2.Controls.Add(this.goalLbl);
-            this.panel2.Controls.Add(this.descriptionLbl);
-            this.panel2.Controls.Add(this.goalBar);
+            this.panel2.Controls.Add(this.lbl_donated);
+            this.panel2.Controls.Add(this.lbl_goal);
+            this.panel2.Controls.Add(this.lbl_description);
+            this.panel2.Controls.Add(this.pbr_goal);
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(854, 586);
             this.panel2.TabIndex = 2;
             // 
-            // dontatedLbl
+            // lbl_donated
             // 
-            this.dontatedLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lbl_donated.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dontatedLbl.AutoSize = true;
-            this.dontatedLbl.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dontatedLbl.ForeColor = System.Drawing.Color.Coral;
-            this.dontatedLbl.Location = new System.Drawing.Point(96, 117);
-            this.dontatedLbl.Name = "dontatedLbl";
-            this.dontatedLbl.Size = new System.Drawing.Size(209, 44);
-            this.dontatedLbl.TabIndex = 4;
-            this.dontatedLbl.Text = "US$ 10,481";
+            this.lbl_donated.AutoSize = true;
+            this.lbl_donated.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_donated.ForeColor = System.Drawing.Color.Coral;
+            this.lbl_donated.Location = new System.Drawing.Point(96, 117);
+            this.lbl_donated.Name = "lbl_donated";
+            this.lbl_donated.Size = new System.Drawing.Size(209, 44);
+            this.lbl_donated.TabIndex = 4;
+            this.lbl_donated.Text = "US$ 10,481";
             // 
-            // goalLbl
+            // lbl_goal
             // 
-            this.goalLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.lbl_goal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.goalLbl.AutoSize = true;
-            this.goalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.goalLbl.Location = new System.Drawing.Point(99, 158);
-            this.goalLbl.Name = "goalLbl";
-            this.goalLbl.Size = new System.Drawing.Size(211, 20);
-            this.goalLbl.TabIndex = 3;
-            this.goalLbl.Text = "pledged of US$ 20,000 goal.";
+            this.lbl_goal.AutoSize = true;
+            this.lbl_goal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_goal.Location = new System.Drawing.Point(99, 158);
+            this.lbl_goal.Name = "lbl_goal";
+            this.lbl_goal.Size = new System.Drawing.Size(211, 20);
+            this.lbl_goal.TabIndex = 3;
+            this.lbl_goal.Text = "pledged of US$ 20,000 goal.";
             // 
-            // descriptionLbl
+            // lbl_description
             // 
-            this.descriptionLbl.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.descriptionLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.lbl_description.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lbl_description.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.descriptionLbl.AutoSize = true;
-            this.descriptionLbl.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.descriptionLbl.Location = new System.Drawing.Point(100, 203);
-            this.descriptionLbl.MaximumSize = new System.Drawing.Size(650, 0);
-            this.descriptionLbl.Name = "descriptionLbl";
-            this.descriptionLbl.Size = new System.Drawing.Size(636, 240);
-            this.descriptionLbl.TabIndex = 2;
-            this.descriptionLbl.Text = resources.GetString("descriptionLbl.Text");
+            this.lbl_description.AutoSize = true;
+            this.lbl_description.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_description.Location = new System.Drawing.Point(100, 203);
+            this.lbl_description.MaximumSize = new System.Drawing.Size(650, 0);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(636, 240);
+            this.lbl_description.TabIndex = 2;
+            this.lbl_description.Text = resources.GetString("lbl_description.Text");
             // 
-            // goalBar
+            // pbr_goal
             // 
-            this.goalBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pbr_goal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.goalBar.BackColor = System.Drawing.SystemColors.Control;
-            this.goalBar.Enabled = false;
-            this.goalBar.Location = new System.Drawing.Point(103, 40);
-            this.goalBar.Margin = new System.Windows.Forms.Padding(0);
-            this.goalBar.MarqueeAnimationSpeed = 0;
-            this.goalBar.Maximum = 20000;
-            this.goalBar.Name = "goalBar";
-            this.goalBar.Size = new System.Drawing.Size(539, 51);
-            this.goalBar.Step = 1;
-            this.goalBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.goalBar.TabIndex = 1;
-            this.goalBar.Value = 10481;
+            this.pbr_goal.BackColor = System.Drawing.SystemColors.Control;
+            this.pbr_goal.Enabled = false;
+            this.pbr_goal.Location = new System.Drawing.Point(103, 40);
+            this.pbr_goal.Margin = new System.Windows.Forms.Padding(0);
+            this.pbr_goal.MarqueeAnimationSpeed = 0;
+            this.pbr_goal.Maximum = 20000;
+            this.pbr_goal.Name = "pbr_goal";
+            this.pbr_goal.Size = new System.Drawing.Size(539, 51);
+            this.pbr_goal.Step = 1;
+            this.pbr_goal.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbr_goal.TabIndex = 1;
+            this.pbr_goal.Value = 10481;
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.donateBtn);
-            this.panel3.Controls.Add(this.orgPic);
+            this.panel3.Controls.Add(this.btn_donate);
+            this.panel3.Controls.Add(this.pic_orgLogo);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel3.Location = new System.Drawing.Point(860, 0);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(313, 586);
             this.panel3.TabIndex = 3;
             // 
-            // donateBtn
+            // btn_donate
             // 
-            this.donateBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(0)))));
-            this.donateBtn.FlatAppearance.BorderSize = 0;
-            this.donateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.donateBtn.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.donateBtn.Location = new System.Drawing.Point(94, 274);
-            this.donateBtn.Name = "donateBtn";
-            this.donateBtn.Size = new System.Drawing.Size(139, 52);
-            this.donateBtn.TabIndex = 2;
-            this.donateBtn.Text = "Donate";
-            this.donateBtn.UseVisualStyleBackColor = false;
+            this.btn_donate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(0)))));
+            this.btn_donate.FlatAppearance.BorderSize = 0;
+            this.btn_donate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_donate.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_donate.Location = new System.Drawing.Point(94, 274);
+            this.btn_donate.Name = "btn_donate";
+            this.btn_donate.Size = new System.Drawing.Size(139, 52);
+            this.btn_donate.TabIndex = 2;
+            this.btn_donate.Text = "Donate";
+            this.btn_donate.UseVisualStyleBackColor = false;
             // 
-            // orgPic
+            // pic_orgLogo
             // 
-            this.orgPic.Image = ((System.Drawing.Image)(resources.GetObject("orgPic.Image")));
-            this.orgPic.Location = new System.Drawing.Point(52, 22);
-            this.orgPic.Name = "orgPic";
-            this.orgPic.Size = new System.Drawing.Size(225, 230);
-            this.orgPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.orgPic.TabIndex = 1;
-            this.orgPic.TabStop = false;
+            this.pic_orgLogo.Image = ((System.Drawing.Image)(resources.GetObject("pic_orgLogo.Image")));
+            this.pic_orgLogo.Location = new System.Drawing.Point(52, 22);
+            this.pic_orgLogo.Name = "pic_orgLogo";
+            this.pic_orgLogo.Size = new System.Drawing.Size(225, 230);
+            this.pic_orgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pic_orgLogo.TabIndex = 1;
+            this.pic_orgLogo.TabStop = false;
             // 
             // organizationInfo
             // 
@@ -158,7 +158,7 @@
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.orgPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_orgLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -166,11 +166,11 @@
         #endregion
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.PictureBox orgPic;
-        private System.Windows.Forms.Button donateBtn;
-        private System.Windows.Forms.Label dontatedLbl;
-        private System.Windows.Forms.Label goalLbl;
-        private System.Windows.Forms.Label descriptionLbl;
-        private System.Windows.Forms.ProgressBar goalBar;
+        private System.Windows.Forms.PictureBox pic_orgLogo;
+        private System.Windows.Forms.Button btn_donate;
+        private System.Windows.Forms.Label lbl_donated;
+        private System.Windows.Forms.Label lbl_goal;
+        private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.ProgressBar pbr_goal;
     }
 }
