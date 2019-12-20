235fbd692ef5686fd736dad615df7be22340e052
TheDigitalPhoenixX <mohamed.xyz@hotmail.com>
2019-12-20 18:50:13 +0200

    refactor: view file structure and control names

---
+++ NPODS/NPODS Non-Profit Organizations Donation System/View/Controls/HeaderControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NPODS_Non_Profit_Organizations_Donation_System
+{
+    public partial class HeaderControl : UserControl
+    {
+        public HeaderControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
