1439e2e5978ed5309fc9b0f53750f269c567d632
Strykar1 <ahmedms.ftw@gmail.com>
2019-12-20 23:09:12 +0200

    feat: changed organization Dashboard to UserControl from Form

--- NPODS/NPODS Non-Profit Organizations Donation System/OrganizationDashboard.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/OrganizationDashboard.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace NPODS_Non_Profit_Organizations_Donation_System {
-    public partial class OrganizationDashboard : Form {
+    public partial class OrganizationDashboard : UserControl {
         private bool isExpanded;
         private readonly int pnl_side_MAXWIDTH;
         private readonly int pnl_side_MINWIDTH;
