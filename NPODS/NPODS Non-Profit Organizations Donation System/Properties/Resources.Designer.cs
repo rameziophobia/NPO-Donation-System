a993437069d1cc93cb7f92dc08e07b6082f04aa7
TheDigitalPhoenixX <mohamed.xyz@hotmail.com>
2019-12-21 13:52:28 +0200

    refactor: FileDatabaseAccess json read

--- NPODS/NPODS Non-Profit Organizations Donation System/Properties/Resources.Designer.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/Properties/Resources.Designer.cs
@@ -61,26 +61,6 @@ namespace NPODS_Non_Profit_Organizations_Donation_System.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] logins {
-            get {
-                object obj = ResourceManager.GetObject("logins", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] Organizations {
-            get {
-                object obj = ResourceManager.GetObject("Organizations", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap placeholder_com_logo4 {
--- NPODS/NPODS Non-Profit Organizations Donation System/controller/DatabaseAccess/FileDatabaseAccess.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/controller/DatabaseAccess/FileDatabaseAccess.cs
@@ -8,108 +8,72 @@ namespace NPODS_Non_Profit_Organizations_Donation_System.controller.DatabaseAcce
 {
     public class FileDatabaseAccess : DatabaseAccess
     {
-
-        private const string PATH_ORG = "../../data/Organizations.json";
-        private const string PATH_DON = "../../data/Donors.json";
-        private const string PATH_LOGINS = "../../data/logins.json";
+        private const string PATH_FOLDER_DATA = "../../data/";
+        private const string PATH_FILE_ORG = PATH_FOLDER_DATA + "Organizations.json";
+        private const string PATH_FILE_DONOR = PATH_FOLDER_DATA + "Donors.json";
+        private const string PATH_FILE_LOGINS = PATH_FOLDER_DATA + "logins.json";
 
         public override Dictionary<string, string> GetLoginInfos()
         {
-            try
-            {
-                return JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(Properties.Resources.logins))
-                    ?? new Dictionary<string, string>();
-            }
-            catch (FileNotFoundException)
-            {
-                //using (StreamWriter streamWriter = File.CreateText(PATH_LOGIN))
-                //{
-                //}
-                //return GetLoginInfos();
-                return null;
-            }
-            catch (JsonReaderException)
-            {
-                //todo log exception
-                //todo raise exception ?
-                return new Dictionary<string, string>();
-            }
+            return readJson<Dictionary<string, string>>(PATH_FILE_LOGINS);
         }
 
         public override List<Organization> loadOrganizations()
         {
-            try
-            {
-                using (StreamReader streamReader = new StreamReader(PATH_ORG))
-                {
-                    return JsonConvert.DeserializeObject<List<Organization>>(streamReader.ReadToEnd()) ?? new List<Organization>();
-                }
-            }
-            catch (FileNotFoundException)
-            {
-                using (StreamWriter streamWriter = File.CreateText(PATH_ORG))
-                {
-                }
-                return loadOrganizations();
-            }
-            catch (JsonReaderException)
-            {
-                //todo log exception
-                //todo raise exception ?
-                return new List<Organization>();
-            }
+            return readJson<List<Organization>>(PATH_FILE_ORG);
         }
 
         public override void SaveOrganizations(List<Organization> organizations)
         {
-            writeJson(PATH_ORG, organizations);
+            writeJson(PATH_FILE_ORG, organizations);
         }
 
         internal override void SaveDonors(List<Donor> donors)
         {
-            writeJson(PATH_DON, donors);
+            writeJson(PATH_FILE_DONOR, donors);
         }
 
-        private static void writeJson(string path, object obj)
+        private void writeJson(string path, object obj)
         {
             using (StreamWriter streamWriter = new StreamWriter(path))
             {
                 streamWriter.Write(JsonConvert.SerializeObject(obj));
             }
         }
-
-        internal override List<Donor> loadDonors()
+        private T readJson<T>(string filePath) where T : class, new()
         {
             try
             {
-                using (StreamReader streamReader = new StreamReader(PATH_DON))
+                using (StreamReader streamReader = new StreamReader(filePath))
                 {
-                    return JsonConvert.DeserializeObject<List<Donor>>(streamReader.ReadToEnd()) ?? new List<Donor>();
+                    return JsonConvert.DeserializeObject<T>(streamReader.ReadToEnd()) ?? new T();
                 }
             }
             catch (FileNotFoundException)
             {
-                using (StreamWriter streamWriter = File.CreateText(PATH_DON))
+                using (StreamWriter streamWriter = File.CreateText(filePath))
                 {
                 }
-                return loadDonors();
+                return new T();
             }
             catch (JsonReaderException)
             {
                 //todo log exception
                 //todo raise exception ?
-                return new List<Donor>();
+                return new T();
             }
+        }
+
+        internal override List<Donor> loadDonors()
+        {
+            return readJson<List<Donor>>(PATH_FILE_DONOR);
         }
 
         internal override void addLogin(string email, string password)
         {
             var logins = GetLoginInfos();
             logins[email] = password;
-            using (StreamWriter streamWriter = new StreamWriter(PATH_LOGINS))
-            {
-                streamWriter.Write(JsonConvert.SerializeObject(logins));
-            }
+            writeJson(PATH_FILE_LOGINS, logins);
         }
     }
 }
