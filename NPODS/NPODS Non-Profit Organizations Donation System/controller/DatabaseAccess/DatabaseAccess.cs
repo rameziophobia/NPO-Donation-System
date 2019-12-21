a31fb21b132b5d2e6bf6f2898a51c8e970800962
Ramez <rameznoshy@gmail.com>
2019-12-21 03:12:08 +0200

    add registrationUtil
    add serializing donors

--- NPODS/NPODS Non-Profit Organizations Donation System/controller/DatabaseAccess/DatabaseAccess.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/controller/DatabaseAccess/DatabaseAccess.cs
@@ -15,7 +15,9 @@ namespace NPODS_Non_Profit_Organizations_Donation_System.controller.DatabaseAcce
         }
 
         public abstract Dictionary<string, string> GetLoginInfos();
-        public abstract Organization[] loadOrganizations();
-        public abstract void SaveOrganizations(Organization[] organizations);
+        public abstract List<Organization>  loadOrganizations();
+        public abstract void SaveOrganizations(List<Organization> organizations);
+        internal abstract List<Donor> loadDonors();
+        internal abstract void SaveDonors(List<Donor> donors);
     }
 }
--- NPODS/NPODS Non-Profit Organizations Donation System/controller/DatabaseAccess/FileDatabaseAccess.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/controller/DatabaseAccess/FileDatabaseAccess.cs
@@ -10,6 +10,7 @@ namespace NPODS_Non_Profit_Organizations_Donation_System.controller.DatabaseAcce
     {
 
         private const string PATH_ORG = "../../data/Organizations.json";
+        private const string PATH_DON = "../../data/Donors.json";
 
         public override Dictionary<string, string> GetLoginInfos()
         {
@@ -34,13 +35,13 @@ namespace NPODS_Non_Profit_Organizations_Donation_System.controller.DatabaseAcce
             }
         }
 
-        public override Organization[] loadOrganizations()
+        public override List<Organization> loadOrganizations()
         {
             try
             {
                 using (StreamReader streamReader = new StreamReader(PATH_ORG))
                 {
-                    return JsonConvert.DeserializeObject<Organization[]>(streamReader.ReadToEnd()) ?? new Organization[0];
+                    return JsonConvert.DeserializeObject<List<Organization>>(streamReader.ReadToEnd()) ?? new List<Organization>();
                 }
             }
             catch (FileNotFoundException)
@@ -54,16 +55,52 @@ namespace NPODS_Non_Profit_Organizations_Donation_System.controller.DatabaseAcce
             {
                 //todo log exception
                 //todo raise exception ?
-                return new Organization[0];
+                return new List<Organization>();
             }
         }
 
-        public override void SaveOrganizations(Organization[] organizations)
+        public override void SaveOrganizations(List<Organization> organizations)
         {
-            using (StreamWriter streamWriter = new StreamWriter(PATH_ORG))
+            writeJson(PATH_ORG, organizations);
+        }
+
+        internal override void SaveDonors(List<Donor> donors)
+        {
+            writeJson(PATH_DON, donors);
+        }
+
+        private static void writeJson(string path, object obj)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(path))
             {
-                streamWriter.Write(JsonConvert.SerializeObject(organizations));
+                streamWriter.Write(JsonConvert.SerializeObject(obj));
             }
         }
+
+        internal override List<Donor> loadDonors()
+        {
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(PATH_DON))
+                {
+                    return JsonConvert.DeserializeObject<List<Donor>>(streamReader.ReadToEnd()) ?? new List<Donor>();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                using (StreamWriter streamWriter = File.CreateText(PATH_DON))
+                {
+                }
+                return loadDonors();
+            }
+            catch (JsonReaderException)
+            {
+                //todo log exception
+                //todo raise exception ?
+                return new List<Donor>();
+            }
+        }
+
+
     }
 }
