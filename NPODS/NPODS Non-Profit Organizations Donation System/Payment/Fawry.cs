fe61343442b999245efabf19ac792a770118c159
Ramez <rameznoshy@gmail.com>
2019-12-21 15:48:42 +0200

    feat: add methods to IPaymentMethod

--- NPODS/NPODS Non-Profit Organizations Donation System/Payment/Fawry.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/Payment/Fawry.cs
@@ -1,6 +1,17 @@
-﻿namespace NPODS_Non_Profit_Organizations_Donation_System.Payment
+﻿using NPODS_Non_Profit_Organizations_Donation_System.Transactions;
+
+namespace NPODS_Non_Profit_Organizations_Donation_System.Payment
 {
     class Fawry : IPaymentMethod
     {
+        public void pay(Transaction transaction)
+        {
+            
+        }
+
+        public bool verifyPayment(Transaction transaction)
+        {
+            return true;
+        }
     }
 }
