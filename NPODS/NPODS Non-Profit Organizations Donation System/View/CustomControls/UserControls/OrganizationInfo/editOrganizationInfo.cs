79dd80bc93e9207d9ecbcde00d69c449de1979a0
Sameh Amnoun <samehamnoun@gmail.com>
2019-12-23 17:59:24 +0200

    feat: checks invalid donation goal

--- NPODS/NPODS Non-Profit Organizations Donation System/View/CustomControls/UserControls/OrganizationInfo/editOrganizationInfo.cs
+++ NPODS/NPODS Non-Profit Organizations Donation System/View/CustomControls/UserControls/OrganizationInfo/editOrganizationInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NPODS_Non_Profit_Organizations_Donation_System.Accounts;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,6 +7,7 @@ namespace NPODS_Non_Profit_Organizations_Donation_System
 {
     public partial class editOrganizationInfo : UserControl
     {
+        public Organization organization;
         public editOrganizationInfo()
         {
             InitializeComponent();
@@ -28,12 +30,28 @@ namespace NPODS_Non_Profit_Organizations_Donation_System
             if (file_chooseImage.ShowDialog() == DialogResult.OK)
             {
                 pic_orgLogo.Image = new Bitmap(file_chooseImage.FileName);
+                organization.LogoFilePath = file_chooseImage.FileName;
             }
         }
 
         private void btn_rmvPhoto_Click(object sender, EventArgs e)
         {
             pic_orgLogo.Image = new Bitmap(Properties.Resources.defaultImage);
+            Console.WriteLine(Properties.Resources.defaultImage);
+        }
+        private void setDonationGoal()
+        {
+            int donationGoal = Convert.ToInt32(txt_goalAmount.Text);
+            if (Convert.ToInt32(txt_goalAmount.Text) >= 0)
+            {
+                organization.DonationGoal.Target = donationGoal;
+            }
+        }
+
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
